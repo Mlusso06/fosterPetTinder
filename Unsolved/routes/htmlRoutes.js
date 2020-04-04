8a6bcfaa2dc6dfc3f5f46299b800220f51d767bf
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-04-04 13:03:34 -0500

    adding petMatch survey and furKeep Logo

--- Unsolved/routes/htmlRoutes.js
+++ Unsolved/routes/htmlRoutes.js
@@ -1,27 +1,44 @@
-var db = require("../models");
+// var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//         msg: "Welcome!",
+//         examples: dbExamples
+//       });
+//     });
+//   });
+
+//   // Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
+//Dependency for the HTML routing for 
+var path = require("path");
+
+// Routing for the exports
+
+module.exports = function(app){
+
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    // if no matching route is found default to home
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
 };
