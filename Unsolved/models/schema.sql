f79ea6592c3d146d7695b0acfc2ae4818b1bf5d0
Celeste Lewis <cdlewis42@gmail.com>
2020-04-04 11:51:13 -0400

    added petComment table

--- Unsolved/models/schema.sql
+++ Unsolved/models/schema.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS cats;
 
 CREATE TABLE cats(
     id int NOT NULL AUTO_INCREMENT,
-    cat_name varchar (255) NOT NULL,
-    cat_gender varchar(1),
-    cat_age varchar(50),
+    cat_name VARCHAR (255) NOT NULL,
+    cat_gender VARCHAR(1),
+    cat_age VARCHAR(50),
     kid_friendly BOOLEAN,
     animal_friendly BOOLEAN,
     PRIMARY KEY (id)
@@ -15,11 +15,19 @@ CREATE TABLE cats(
 
 CREATE TABLE dogs(
     id int NOT NULL AUTO_INCREMENT,
-    dog_name varchar (255) NOT NULL,
-    dog_gender varchar(1),
-    dog_age varchar(50),
+    dog_name VARCHAR (255) NOT NULL,
+    dog_gender VARCHAR(1),
+    dog_age VARCHAR(50),
     kid_friendly BOOLEAN,
     animal_friendly BOOLEAN,
     PRIMARY KEY (id)
 )
 
+CREATE TABLE petComments(
+    id INT NOT NULL AUTO INCREMENT,
+    author VARCHAR(50) NOT NULL,
+    comment VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+)
