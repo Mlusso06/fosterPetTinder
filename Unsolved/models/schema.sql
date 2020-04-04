54c3a837a2b4721f7871efe828dd1fa870ee60eb
Celeste Lewis <cdlewis42@gmail.com>
2020-04-04 11:39:41 -0400

    This is the better schema and seed

--- Unsolved/models/schema.sql
+++ Unsolved/models/schema.sql
@@ -1,5 +1,25 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE IF NOT EXISTS fosterPets_db;
+USE fosterPets_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP TABLE IF EXISTS cats;
+
+CREATE TABLE cats(
+    id int NOT NULL AUTO_INCREMENT,
+    cat_name varchar (255) NOT NULL,
+    cat_gender varchar(1),
+    cat_age varchar(50),
+    kid_friendly BOOLEAN,
+    animal_friendly BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dogs(
+    id int NOT NULL AUTO_INCREMENT,
+    dog_name varchar (255) NOT NULL,
+    dog_gender varchar(1),
+    dog_age varchar(50),
+    kid_friendly BOOLEAN,
+    animal_friendly BOOLEAN,
+    PRIMARY KEY (id)
+)
+
---
+++ Unsolved/models/seeds.sql
@@ -0,0 +1,23 @@
+USE fosterPets_db;
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Cheeseburger", "m", "Young", true, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Charles", "m", "Old", true, false);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Bobo", "f", "Adult", false, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Yeezy", "m", "Young", true, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Tater", "f", "Young", true, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Mellow", "m", "Adult", false, false);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Gertie", "f", "Old", false, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Ketchup", "f", "Young", true, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Marshall", "m", "Adult", true, true);
+INSERT INTO cats (cat_name, cat_gender, cat_age, kid_friendly, animal_friendly) VALUES ("Portland", "m", "Old", true, true);
+
+
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Horton", "m", "Young", true, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Mooney", "m", "Old", true, false);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Bubba", "f", "Adult", false, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Banjo", "m", "Young", true, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Peep", "f", "Young", true, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Loki", "m", "Adult", false, false);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Bellow", "f", "Old", false, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Ronald", "f", "Young", true, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Harold", "m", "Adult", true, true);
+INSERT INTO dogs (dog_name, dog_gender, dog_age, kid_friendly, animal_friendly) VALUES ("Potato", "m", "Old", true, true);
