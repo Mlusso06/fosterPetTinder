6b40b738d5e3bf2dc89f2ddbcb4bb9d7e216d3e8
Celeste Lewis <cdlewis42@gmail.com>
2020-04-04 12:32:23 -0400

    added create and update ORM functions

--- Unsolved/config/orm.js
+++ Unsolved/config/orm.js
@@ -1,9 +1,23 @@
 var connection = require('config.json')
 var orm = {
-    findAll(tableName,cb){
+    findAll(tableName, cb) {
         var queryString = "SELECT * FROM ??;";
-        connection.query(queryString,[tableName], function(err,result){
+        connection.query(queryString, [tableName], function (err, result) {
             if (err) throw err;
+            cb(result)
+        })
+    },
+    createComment(tableName, columnName, values, cb) {
+        var queryString = "INSERT INTO ?? (??, ??) VALUES (?,?)";
+        connection.query(queryString[tableName, columnName[0], columnName[1], values[0], values[1]], function (err, result) {
+            if (err) throw err
+            cb(result)
+        })
+    },
+    updateComment(tableName, columnName, values, cb) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? =?"
+        connection.query(queryString, [tableName, columnName[0], values[0], columnName[1], values[1]], function (err, result) {
+            if (err) throw err
             cb(result)
         })
     }
