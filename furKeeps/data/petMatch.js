47abec37f4c57e31fcb5e46f4dda4583192613ad
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-04-06 21:18:45 -0500

    updated changes

--- furKeeps/data/petMatch.js
+++ furKeeps/data/petMatch.js
@@ -1,32 +1,32 @@
 var petMatch = [
     {
         name: "dog1",
-        photo: "https://a.wattpad.com/cover/64591564-352-k956444.jpg",
+        photo: "",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
     },
     {
         name: "dog2",
-        photo: "http://www.cgw.com/images/cmssys/thumbnails/BudLightHBO580-580345-13-TransparentWhite-15235-580345-13-TransparentWhite-1.jpg",
+        photo: "",
         scores: [5, 4, 2, 3, 1, 3, 1, 4, 4, 2 ]
     },
     {
         name: "dog3",
-        photo: "https://www.awesomeinventions.com/wp-content/uploads/2019/10/buddy.jpg",
+        photo: "",
         scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4 ]
     },
     {
         name: "dog4",
-        photo: "https://www.rollingstone.com/wp-content/uploads/2018/06/mark-wahlberg-boogie-nights-1ff3f595-8136-4e2f-a31a-edcaf6413535.jpg?resize=1800,1200&w=1200",
+        photo: "",
         scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5 ]
     },
     {
         name: "dog5",
-        photo: "http://hackaday.com/wp-content/uploads/2008/08/tomanderson.jpg",
+        photo: "",
         scores: [2, 3, 3, 1, 5, 3, 5, 1, 3, 1 ]
     },
     {
         name: "dog6",
-        photo: "https://espnpressroom.com/us/files/2018/02/RS562652_20171114_PE3_6710-e1518622066935-780x405.jpg",
+        photo: "",
         scores: [3, 5, 3, 2, 5, 5, 3, 2, 1, 3 ]
     }
 ];
