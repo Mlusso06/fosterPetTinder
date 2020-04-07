e405a9394b2c9503bb1e3e6aca8bd18af18804dc
d-taylor6403 <dannielletaylor.com>
2020-04-06 22:26:42 -0500

    updates to index.handlebars, htmlroutes and styles folder

---
+++ furKeeps/public/js/main.js
@@ -0,0 +1,26 @@
+
+//Carousel Function
+$('.owl-carousel.owl-theme').owlCarousel({
+    items: 3,
+    center: true,
+    dots: true,
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    nav: true,
+    autoHeight: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
