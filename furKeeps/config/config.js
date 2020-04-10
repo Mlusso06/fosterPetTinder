f995b079e3bd6c4d6849b0c52d9754ddba09326c
d-taylor6403 <dannielletaylor.com>
2020-04-10 11:30:19 -0500

    changes to animal.js, controller.js and commentRoutes.js

--- furKeeps/config/config.js
+++ furKeeps/config/config.js
@@ -3,12 +3,12 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "Mlusso06",
-  password: "thispasswrd",
+  user: "d.taylor",
+  password: "12345",
   database: "fosterPets_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- furKeeps/models/schema.sql
+++ furKeeps/models/schema.sql
@@ -1,30 +1,25 @@
 CREATE DATABASE IF NOT EXISTS fosterPets_db;
 USE fosterPets_db;
 
-DROP TABLE IF EXISTS cats;
-
-CREATE TABLE cats(
+CREATE DATABASE IF NOT EXISTS fosterPets_db;
+USE fosterPets_db;
+​
+DROP TABLE IF EXISTS pets;
+​
+CREATE TABLE pets(
     id int NOT NULL AUTO_INCREMENT,
-    cat_name VARCHAR (255) NOT NULL,
-    cat_gender VARCHAR(1),
-    cat_age VARCHAR(50),
+    pet_name VARCHAR (255) NOT NULL,
+    pet_type VARCHAR (50) NOT NULL,
+    pet_gender VARCHAR(1),
+    pet_age VARCHAR(50),
     kid_friendly BOOLEAN,
     animal_friendly BOOLEAN,
-    cat_photo VARCHAR (500) NOT NULL,
+    match_score INT NOT NULL,
+    pet_photo VARCHAR (500) NOT NULL,
+    match_score INT NOT NULL,
     PRIMARY KEY (id)
 );
-
-CREATE TABLE dogs(
-    id int NOT NULL AUTO_INCREMENT,
-    dog_name VARCHAR (255) NOT NULL,
-    dog_gender VARCHAR(1),
-    dog_age VARCHAR(50),
-    kid_friendly BOOLEAN,
-    animal_friendly BOOLEAN,
-    dog_photo VARCHAR (500) NOT NULL,
-    PRIMARY KEY (id)
-);
-
+​
 CREATE TABLE petComments(
     id INT NOT NULL AUTO_INCREMENT,
     author VARCHAR(50) NOT NULL,
@@ -33,3 +28,4 @@ CREATE TABLE petComments(
     last_updated TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
+
