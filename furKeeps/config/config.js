8421cb51bdd831d675347f50a970abbc06168c8c
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-04-11 14:48:01 -0500

    adding jaws connection, changed config.js

--- furKeeps/config/config.js
+++ furKeeps/config/config.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "d.taylor",
-  password: "12345",
-  database: "fosterPets_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DBhost,
+    port: 3306,
+    user: process.env.DBuser,
+    password: process.env.DBPass,
+    database: "fosterPets_db"
+  })
+}
 
 connection.connect(function (err) {
   if (err) {
