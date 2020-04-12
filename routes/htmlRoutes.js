4a053e12a7c2f6e455eb6af27e7ae5eee60432ca
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-04-12 14:59:51 -0500

    changing the path for the survey, and adding require for the surver

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,31 +1,4 @@
-// var db = require("../models");
 
-// module.exports = function(app) {
-//   // Load index page
-//   app.get("/", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.render("index", {
-//         msg: "Welcome!",
-//         examples: dbExamples
-//       });
-//     });
-//   });
-
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
-
-//   // Render 404 page for any unmatched routes
-//   app.get("*", function(req, res) {
-//     res.render("404");
-//   });
-// };
-//Dependency for the HTML routing for 
 var path = require("path");
 
 // Routing for the exports
